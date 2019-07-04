eb986565f9b844cf7b444ee9ae80df1c845250f3
jjgvalenzuela84 <jjgvalenzuela@gmail.com>
2019-07-04 19:00:18 +0200

    pruebas commit

--- HolaMundoApp/app/src/main/java/com/example/holamundoapp/MainActivity.java
+++ HolaMundoApp/app/src/main/java/com/example/holamundoapp/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void saludar(View view){
          Toast.makeText(this, "Hola Tontuna!!!",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hola Tontuna!!!",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hola Tontuna!!!",Toast.LENGTH_SHORT).show();Toast.makeText(this, "Hola Tontuna!!!",Toast.LENGTH_SHORT).show();
     }
 
 

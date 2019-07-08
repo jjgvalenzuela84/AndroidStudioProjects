6899a40622d3b68a4413ce66649e0b78fe271123
jjgvalenzuela84 <jjgvalenzuela@gmail.com>
2019-07-08 19:05:44 +0200

    .

---
+++ appSegundoActivity_2/app/src/main/java/cursoandroid/jjgv/appsegundoactivity_2/Bienvenido.java
@@ -0,0 +1,14 @@
+package cursoandroid.jjgv.appsegundoactivity_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bienvenido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenido);
+    }
+}

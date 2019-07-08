fc49b2ac1e4898d81a655fec9bae8191c21dd11c
jjgvalenzuela84 <jjgvalenzuela@gmail.com>
2019-07-08 12:14:39 +0200

    .

---
+++ appControlListView/app/src/main/java/cursoandroid/jjgv/appcontrollistview/MainActivity.java
@@ -0,0 +1,77 @@
+package cursoandroid.jjgv.appcontrollistview;
+
+import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvl;
+    private ListView lvl;
+    private String[] paises={"Argentina","Chile","Paraguay","Bolivia","Peru",
+            "Ecuador","Brasil","Colombia","Venezuela","Uruguay"};
+    private String[] habitantes={"40000000","17000000","6500000","10000000","30000000",
+            "14000000","183000000","44000000","29000000","3500000"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+       FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        tvl=findViewById(R.id.tv1);
+        lvl=findViewById(R.id.lv1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, paises);
+        lvl.setAdapter(adapter);
+
+        lvl.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                tvl.setText("Poblacion de "+ lvl.getItemAtPosition(position) + " es " + habitantes[position]);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

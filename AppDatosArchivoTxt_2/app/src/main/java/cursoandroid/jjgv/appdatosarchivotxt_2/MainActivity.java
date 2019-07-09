e9d572daf7aa1f9eca85fac1e19d9a2967eb130c
jjgvalenzuela84 <jjgvalenzuela@gmail.com>
2019-07-09 20:32:22 +0200

    Calendario Actividades

---
+++ AppDatosArchivoTxt_2/app/src/main/java/cursoandroid/jjgv/appdatosarchivotxt_2/MainActivity.java
@@ -0,0 +1,22 @@
+package cursoandroid.jjgv.appdatosarchivotxt_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void guardar(View view){
+
+    }
+    public void recuperar(View view){
+
+    }
+}
